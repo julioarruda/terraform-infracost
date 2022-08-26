2ea44e3e6d4fd47e04d955b38bc1c306be645e42
Julio Arruda <julio.arruda@outlook.com>
2022-08-26 20:12:47 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -101,7 +101,7 @@ resource "azurerm_linux_virtual_machine" "myterraformvm" {
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.myterraformnic.id]
-  size                  = "Standard_B2s"
+  size                  = "Standard_NDv2"
 
   os_disk {
     name                 = "myOsDisk"
